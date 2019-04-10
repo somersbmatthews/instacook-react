9d3fdac48b447407f0c961db14299f7cade9ddc1
Somers Matthews <somersbmatthews@gmail.com>
2019-04-10 17:52:10 -0500

    added jsx for landing page in [App.js]

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <h1>Instacook</h1>
+        <h2>Search for a recipe...</h2>
       </div>
     );
   }
