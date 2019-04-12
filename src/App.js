b897c786cf5a446af5cd9c9d5ad9b7ddfd9552a3
Somers Matthews <somersbmatthews@gmail.com>
2019-04-12 00:08:10 -0500

    autosuggest rendering but not yet tested

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import IntegrationAutosuggest from './components/AutoSuggest';
 
 class App extends Component {
   render() {
@@ -7,6 +8,8 @@ class App extends Component {
       <div className="App">
         <h1>Instacook</h1>
         <h2>Search for a recipe...</h2>
+
+        <IntegrationAutosuggest />
       </div>
     );
   }
