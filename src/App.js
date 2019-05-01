9c757a2a1f08bd27234d1774c03023a16b711575
Somers Matthews <somersbmatthews@gmail.com>
2019-04-30 23:39:10 -0500

    changed dimensions for video

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
         <h2>Search for a recipe...</h2>
 
         <IntegrationAutosuggest />
-        <video width="320" height="240" controls>
+        <video width="600" height="400" controls>
         <source src="movie.mp4" type="video/mp4">
         <source src="movie.ogg" type="video/ogg">
         Your browser does not support the video tag.
