5f09495e769f33fd5c0814d25ec6dc38a9c97d1c
Somers Matthews <somersbmatthews@gmail.com>
2019-04-30 23:15:23 -0500

    added sample html5 video element to [App.js]

--- src/App.js
+++ src/App.js
@@ -10,6 +10,11 @@ class App extends Component {
         <h2>Search for a recipe...</h2>
 
         <IntegrationAutosuggest />
+        <video width="320" height="240" controls>
+        <source src="movie.mp4" type="video/mp4">
+        <source src="movie.ogg" type="video/ogg">
+        Your browser does not support the video tag.
+        </video>
       </div>
     );
   }
